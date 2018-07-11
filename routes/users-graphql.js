a3ba6bc10bdf716841d55255fae2047e3abfa93f
harris-miller <harrismillerconsulting@gmail.com>
2018-07-11 13:56:52 -0600

    while I like graphQL, for the sake of this demo, going to stick to what i know, REST

---
+++ routes/users-graphql.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const createError = require('http-errors');
+const User = require('../models/user');
+const { buildSchema } = require('graphql');
+const graphqlHTTP = require('express-graphql');
+
+const userSchema = buildSchema(`
+  type Query {
+    user(id: ID!): User
+    users: [User]
+  },
+  type Mutation {
+    updateUser(id: ID!, displayname: String, email: String, profilePicUrl: String): User
+  },
+  type User {
+    id: ID
+    displayname: String
+    email: String
+    profilePicUrl: String
+    searchname: String
+    searchnameReverse: String
+    createdAt: String
+    updatedAt: String,
+    posts: [Post]
+  },
+  type Post {
+    id: ID
+    userId: ID
+    text: String
+    fullUrl: String
+  }
+`);
+
+function getUser({ id }) {
+  return User
+    .query({ where: { id }})
+    .fetch({ withRelated: 'posts' })
+    .then(user => {
+      if (!user) {
+        return new createError.NotFound();
+      }
+
+      return user.toJSON();
+    });
+}
+
+function getUsers() {
+  return User
+    .fetchAll({ withRelated: 'posts' })
+    .then(users => {
+      return users.toJSON();
+    });
+}
+
+function updateUser({ id, displayname, email, profilePicUrl }) {
+  const saveObj = {};
+  id && (saveObj['id'] = id);
+  displayname && (saveObj['displayname'] = displayname);
+  email && (saveObj['email'] = email);
+  profilePicUrl && (saveObj['profile_pic_url'] = profilePicUrl);
+
+  return User
+    .query({ where: { id }})
+    .fetch()
+    .then(user => {
+      if (!user) {
+        return new createError.NotFound();
+      }
+
+      return user.save(saveObj, { patch: true });
+    }).then(user =>
+      user.toJSON()
+    );
+}
+
+const root = {
+  user: getUser,
+  users: getUsers,
+  updateUser
+};
+
+module.exports = graphqlHTTP({
+  schema: userSchema,
+  rootValue: root,
+  graphiql: process.env.NODE_ENV === 'development'
+});
--- routes/users.js
+++ routes/users.js
@@ -1,87 +1,73 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const createError = require('http-errors');
+const router = new express.Router();
 const User = require('../models/user');
-const { buildSchema } = require('graphql');
-const graphqlHTTP = require('express-graphql');
+// const authenticate = require('../middleware/authenticate');
 
-const userSchema = buildSchema(`
-  type Query {
-    user(id: ID!): User
-    users: [User]
-  },
-  type Mutation {
-    updateUser(id: ID!, displayname: String, email: String, profilePicUrl: String): User
-  },
-  type User {
-    id: ID
-    displayname: String
-    email: String
-    profilePicUrl: String
-    searchname: String
-    searchnameReverse: String
-    createdAt: String
-    updatedAt: String,
-    posts: [Post]
-  },
-  type Post {
-    id: ID
-    userId: ID
-    text: String
-    fullUrl: String
+// temp test route
+router.route('/').get((req, res) =>
+  User
+    // .fetchAll({ columns: ['id', 'displayname', 'email', 'created_at', 'updated_at'] })
+    .fetchAll()
+    .then(results => {
+      res.json(results);
+    })
+);
+
+router.route('/').post((req, res, next) => {
+  const { email, password, displayname } = req.body;
+
+  if (!email || !password || !displayname) {
+    return next(new createError.BadRequest('Invalid Credentials'));
   }
-`);
 
-function getUser({ id }) {
-  return User
-    .query({ where: { id }})
-    .fetch({ withRelated: 'posts' })
-    .then(user => {
-      if (!user) {
-        return new createError.NotFound();
-      }
-
-      return user.toJSON();
-    });
-}
-
-function getUsers() {
-  return User
-    .fetchAll({ withRelated: 'posts' })
-    .then(users => {
-      return users.toJSON();
-    });
-}
-
-function updateUser({ id, displayname, email, profilePicUrl }) {
-  const saveObj = {};
-  id && (saveObj['id'] = id);
-  displayname && (saveObj['displayname'] = displayname);
-  email && (saveObj['email'] = email);
-  profilePicUrl && (saveObj['profile_pic_url'] = profilePicUrl);
+  const passwordDigest = bcrypt.hashSync(password, 10);
 
   return User
-    .query({ where: { id }})
-    .fetch()
-    .then(user => {
-      if (!user) {
-        return new createError.NotFound();
-      }
-
-      return user.save(saveObj, { patch: true });
-    }).then(user =>
-      user.toJSON()
-    );
-}
-
-const root = {
-  user: getUser,
-  users: getUsers,
-  updateUser
-};
-
-module.exports = graphqlHTTP({
-  schema: userSchema,
-  rootValue: root,
-  graphiql: process.env.NODE_ENV === 'development'
+    .forge({
+      displayname,
+      email,
+      passwordDigest
+    }, {
+      hasTimestamps: true
+    })
+    .save()
+    .then(newUser => {
+      const { passwordDigest: _, ...rest } = newUser.attributes;
+      res.status(201);
+      res.json(rest);
+    }).catch(err => next(new createError.InternalServerError(err)));
 });
+
+router.route('/:id').get((req, res, next) => {
+  const { id } = req.params;
+
+  User
+    .query({ where: { id: parseInt(id, 10) }})
+    .fetch({ columns: ['id', 'displayname', 'profile_pic_url']})
+    .then(user => {
+      if (!user) {
+        return res.next(new createError.NotFound());
+      }
+
+      return res.json(user);
+    })
+});
+
+router.route('/:id/posts').get((req, res, next) => {
+  const { id } = req.params;
+
+  User
+    .query({ where: { id: parseInt(id, 10) }})
+    .fetch({ withRelated: [{ posts: query => { query.select('id', 'text', 'full_url', 'created_at', 'updated_at', 'user_id'); } }] })
+    .then(user => {
+      if (!user) {
+        return next(new createError.NotFound());
+      }
+
+      return res.json(user.related('posts'));
+    });
+});
+
+module.exports = router;
